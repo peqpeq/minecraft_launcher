39c7d9e85ca19966db2c0396f8124e9cf6ad089f
peqpeq <sapnikz@gmail.com>
2022-01-16 05:29:31 +0200

    First commit

---
+++ Todo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+using ReactiveUI;
+using Todo.Models;
+using Todo.Services;
+
+namespace Todo.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        ViewModelBase content;
+
+        public MainWindowViewModel(Database db)
+        {
+            Content = List = new TodoListViewModel(db.GetItems());
+        }
+
+        public ViewModelBase Content
+        {
+            get => content;
+            private set => this.RaiseAndSetIfChanged(ref content, value);
+        }
+
+        public TodoListViewModel List { get; }
+
+        public void AddItem()
+        {
+            var vm = new AddItemViewModel();
+
+            Observable.Merge(
+                    vm.Ok,
+                    vm.Cancel.Select(_ => (TodoItem)null))
+                .Take(1)
+                .Subscribe(model =>
+                {
+                    if (model != null)
+                    {
+                        List.Items.Add(model);
+                    }
+
+                    Content = List;
+                });
+
+            Content = vm;
+        }
+    }
+}
---
+++ Todo/Views/AddItemView.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Todo.Views
+{
+    public partial class AddItemView : UserControl
+    {
+        public AddItemView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
